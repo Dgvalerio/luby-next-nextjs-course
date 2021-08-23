893d5aee4f04f69de9a4e06a6550a6ba881dbe25
Davi Gonçalves Valério <al.davigvalerio@gmail.com>
2021-08-23 11:23:12 -0300

    77. Working on the "All Events" Page

--- pages/events/index.tsx
+++ pages/events/index.tsx
@@ -1,5 +1,16 @@
 import { FC } from 'react';
 
-const AllEventsPage: FC = () => <h1>All Events</h1>;
+import EventList from '../../components/events/EventList';
+import { getAllEvents } from '../../dummy-data';
+
+const AllEventsPage: FC = () => {
+  const allEvents = getAllEvents();
+
+  return (
+    <div>
+      <EventList items={allEvents} />
+    </div>
+  );
+};
 
 export default AllEventsPage;
--- pages/index.tsx
+++ pages/index.tsx
@@ -8,8 +8,6 @@ const HomePage: FC = () => {
 
   return (
     <div>
-      <h1>The Home Page</h1>
-
       <EventList items={featuredEvents} />
     </div>
   );
