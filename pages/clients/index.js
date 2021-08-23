d2809256b4b7ad3b127b22a12c42ead8694e1bd3
Davi Gonçalves Valério <al.davigvalerio@gmail.com>
2021-08-23 08:52:26 -0300

    Video 61 - Navigating To Dynamic Routes

--- pages/clients/index.js
+++ pages/clients/index.js
@@ -1,7 +1,21 @@
+import Link from 'next/link';
+
 const ClientsPage = () => {
+  const clients = [
+    { id: 'max', name: 'Maximilian' },
+    { id: 'manu', name: 'Manuel' },
+  ];
+
   return (
     <div>
       <h1>The Clients Page</h1>
+      <ul>
+        {clients.map(({ name, id }) => (
+          <li key={id}>
+            <Link href={`/clients/${id}`}>{name}</Link>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
