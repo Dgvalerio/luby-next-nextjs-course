11a7c8e596ea424c6ab0797171bb86c440a3b84e
Davi Gonçalves Valério <al.davigvalerio@gmail.com>
2021-08-25 13:04:38 -0300

    139. Preparing the Frontend Form

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,38 @@
+import { FormEvent, useRef } from 'react';
+
 import { NextPage } from 'next';
 
-const Home: NextPage = () => <h1>Hello</h1>;
+const Home: NextPage = () => {
+  const emailInputRef = useRef<HTMLInputElement>(null);
+  const feedbackInputRef = useRef<HTMLTextAreaElement>(null);
+
+  const submitFormHandler = (event: FormEvent) => {
+    event.preventDefault();
+
+    const email = emailInputRef.current.value;
+    const feedback = feedbackInputRef.current.value;
+  };
+
+  return (
+    <div>
+      <h1>The Home Page</h1>
+      <form onSubmit={submitFormHandler}>
+        <div>
+          <label htmlFor="email">
+            <span>Your Email Address</span>
+            <input type="email" id="email" ref={emailInputRef} />
+          </label>
+        </div>
+        <div>
+          <label htmlFor="email">
+            <span>Your Feedback</span>
+            <textarea id="feedback" rows={5} ref={feedbackInputRef} />
+          </label>
+        </div>
+        <button type="submit">Send Feedback</button>
+      </form>
+    </div>
+  );
+};
 
 export default Home;
