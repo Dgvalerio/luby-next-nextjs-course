512854bd3371af2f8417993c42809b6aa4918a71
Davi Gonçalves Valério <al.davigvalerio@gmail.com>
2021-08-25 10:19:34 -0300

    125. Configuring the "head" Content

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
 import { GetStaticProps, NextPage } from 'next';
+import Head from 'next/head';
 
 import EventList from '../components/events/event-list';
 import { getFeaturedEvents, IEvent } from '../helpers/api-utils';
@@ -9,6 +10,13 @@ interface HomePageProps {
 
 const HomePage: NextPage<HomePageProps> = ({ events }) => (
   <div>
+    <Head>
+      <title>NextJS Events</title>
+      <meta
+        name="description"
+        content="Find a lot of great events that allow you yo evolve..."
+      />
+    </Head>
     <EventList items={events} />
   </div>
 );
