6071124d7c4060421c91f4ad26c72bd480438acc
Davi Gonçalves Valério <al.davigvalerio@gmail.com>
2021-08-25 10:51:48 -0300

    128. Working with the "_app.js" File (and Why)

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,11 +3,15 @@ import '../styles/globals.css';
 
 import { NextPage } from 'next';
 import { AppProps } from 'next/app';
+import Head from 'next/head';
 
 import Layout from '../components/layout/layout';
 
 const MyApp: NextPage<AppProps> = ({ Component, pageProps }) => (
   <Layout>
+    <Head>
+      <meta name="viewport" content="initial-scala=1.0, width=device-width" />
+    </Head>
     <Component {...pageProps} />
   </Layout>
 );
