1e85087f9947b1f25369d1fab3b453242a3a6f16
Davi Gonçalves Valério <al.davigvalerio@gmail.com>
2021-08-26 19:58:26 -0300

    167. Using Context Data In Components

--- components/layout/layout.tsx
+++ components/layout/layout.tsx
@@ -1,12 +1,27 @@
+import { useContext } from 'react';
+
 import { NextPage } from 'next';
 
+import NotificationContext from '../../store/notification-context';
+import Notification from '../ui/notification';
 import MainHeader from './main-header';
 
-const Layout: NextPage = ({ children }) => (
-  <>
-    <MainHeader />
-    <main>{children}</main>
-  </>
-);
+const Layout: NextPage = ({ children }) => {
+  const { notification } = useContext(NotificationContext);
+
+  return (
+    <>
+      <MainHeader />
+      <main>{children}</main>
+      {notification && (
+        <Notification
+          title={notification.title}
+          message={notification.message}
+          status={notification.status}
+        />
+      )}
+    </>
+  );
+};
 
 export default Layout;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,7 +6,6 @@ import { AppProps } from 'next/app';
 import Head from 'next/head';
 
 import Layout from '../components/layout/layout';
-import Notification from '../components/ui/notification';
 import { NotificationContextProvider } from '../store/notification-context';
 
 const MyApp: NextPage<AppProps> = ({ Component, pageProps }) => (
@@ -18,10 +17,8 @@ const MyApp: NextPage<AppProps> = ({ Component, pageProps }) => (
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
       <Component {...pageProps} />
-      <Notification title="Test" message="This is a test." status="success" />
     </Layout>
   </NotificationContextProvider>
-
 );
 
 export default MyApp;
