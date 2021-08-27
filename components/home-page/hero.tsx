51190802e48c5c4a2e647f1ef98a9bc19b6a41b9
Davi Gonçalves Valério <al.davigvalerio@gmail.com>
2021-08-27 10:50:06 -0300

    176. Adding The "Hero" Component

--- components/home-page/hero.tsx
+++ components/home-page/hero.tsx
@@ -1,5 +1,23 @@
 import { NextPage } from 'next';
+import Image from 'next/image';
 
-const Hero: NextPage = () => <></>;
+import classes from './hero.module.css';
+
+const Hero: NextPage = () => (
+  <section className={classes.hero}>
+    <div className={classes.image}>
+      <Image
+        src="/images/site/davi.jpeg"
+        alt="An image showing Davi"
+        width={300}
+        height={300}
+      />
+    </div>
+    <h1>Hi, I&apos;m Davi</h1>
+    <p>
+      I blog about web development - especially frontend frameworks like React.
+    </p>
+  </section>
+);
 
 export default Hero;
