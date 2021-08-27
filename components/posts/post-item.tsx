d44f22e9a748c06b2b8b2737a9c694bd8cd590d9
Davi Gonçalves Valério <al.davigvalerio@gmail.com>
2021-08-27 11:40:28 -0300

    180. Adding A Post Grid & Post Items

--- components/posts/post-item.tsx
+++ components/posts/post-item.tsx
@@ -1,5 +1,38 @@
 import { NextPage } from 'next';
+import Image from 'next/image';
+import Link from 'next/link';
 
-const PostItem: NextPage<{ post? }> = () => <li />;
+import { IPost } from '../../types/interfaces';
+import classes from './post-item.module.css';
+
+const PostItem: NextPage<{ post: IPost }> = ({
+  post: { excerpt, image, title, date, slug },
+}) => {
+  const formattedDate = new Date(date).toLocaleDateString('pt-BR', {
+    day: 'numeric',
+    month: 'long',
+    year: 'numeric',
+  });
+
+  const imagePath = `/images/posts/${slug}/${image}`;
+
+  return (
+    <li className={classes.post}>
+      <Link href={slug} passHref>
+        {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
+        <a>
+          <div className={classes.image}>
+            <Image src={imagePath} alt={title} width={300} height={200} />
+          </div>
+          <div className={classes.content}>
+            <h3>{title}</h3>
+            <time>{formattedDate}</time>
+            <p>{excerpt}</p>
+          </div>
+        </a>
+      </Link>
+    </li>
+  );
+};
 
 export default PostItem;
