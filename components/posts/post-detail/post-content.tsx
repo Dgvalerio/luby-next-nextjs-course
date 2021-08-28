ef34206727e8e093c0df5ba22dae54cb9449b34d
Davi Gonçalves Valério <al.davigvalerio@gmail.com>
2021-08-28 11:48:00 -0300

    190. Rendering Images With The "Next Image" Component (From Markdown)

--- components/posts/post-detail/post-content.tsx
+++ components/posts/post-detail/post-content.tsx
@@ -1,6 +1,9 @@
 import { NextPage } from 'next';
+import Image from 'next/image';
 
 import ReactMarkdown from 'react-markdown';
+import { SpecialComponents } from 'react-markdown/lib/ast-to-react';
+import { NormalComponents } from 'react-markdown/lib/complex-types';
 
 import { IPost } from '../../../types/interfaces';
 import classes from './post-content.module.css';
@@ -11,10 +14,47 @@ const PostContent: NextPage<{ post: IPost }> = ({
 }) => {
   const imagePath = `/images/posts/${slug}/${image}`;
 
+  const customRenderers: Partial<
+    Omit<NormalComponents, keyof SpecialComponents> & SpecialComponents
+  > = {
+    // img({ src, alt }) {
+    //   return (
+    //     <Image
+    //       src={`/images/posts/${slug}/${src}`}
+    //       alt={alt}
+    //       width={600}
+    //       height={300}
+    //     />
+    //   );
+    // },
+    p({ node, children }) {
+      if (
+        node.children[0].type === 'element' &&
+        node.children[0].tagName === 'img'
+      ) {
+        const {
+          properties: { src, alt },
+        } = node.children[0];
+
+        return (
+          <div className={classes.image}>
+            <Image
+              src={`/images/posts/${slug}/${src}`}
+              alt={`${alt}`}
+              width={600}
+              height={300}
+            />
+          </div>
+        );
+      }
+      return <p>{children}</p>;
+    },
+  };
+
   return (
     <article className={classes.content}>
       <PostHeader image={imagePath} title={title} />
-      <ReactMarkdown>{content}</ReactMarkdown>
+      <ReactMarkdown components={customRenderers}>{content}</ReactMarkdown>
     </article>
   );
 };
