f8b551ae43d6f2e51e88f00e0484593743713dcc
Davi Gonçalves Valério <al.davigvalerio@gmail.com>
2021-08-30 12:22:04 -0300

    227. Using the "next-auth" Session Provider Component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,14 +2,17 @@
 import '../styles/globals.css';
 
 import { NextPage } from 'next';
+import { Provider } from 'next-auth/client';
 import { AppProps } from 'next/app';
 
 import Layout from '../components/layout/layout';
 
 const MyApp: NextPage<AppProps> = ({ Component, pageProps }) => (
-  <Layout>
-    <Component {...pageProps} />
-  </Layout>
+  <Provider session={pageProps.session}>
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  </Provider>
 );
 
 export default MyApp;
