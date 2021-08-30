abf7af731f7fa4f4601afa2e29df14113d8446f2
Davi Gonçalves Valério <al.davigvalerio@gmail.com>
2021-08-30 12:55:13 -0300

    231. Sending a "Change Password" Request From The Frontend

--- components/profile/profile-form.tsx
+++ components/profile/profile-form.tsx
@@ -1,22 +1,46 @@
 /* eslint-disable jsx-a11y/label-has-associated-control */
+import { FormEvent, useRef } from 'react';
+
 import { NextPage } from 'next';
 
+import { ChangePasswordPostRequest } from '../../pages/api/user/change-password';
 import classes from './profile-form.module.css';
 
-const ProfileForm: NextPage = () => (
-  <form className={classes.form}>
-    <div className={classes.control}>
-      <label htmlFor="new-password">New Password</label>
-      <input type="password" id="new-password" />
-    </div>
-    <div className={classes.control}>
-      <label htmlFor="old-password">Old Password</label>
-      <input type="password" id="old-password" />
-    </div>
-    <div className={classes.action}>
-      <button type="submit">Change Password</button>
-    </div>
-  </form>
-);
+const ProfileForm: NextPage<{
+  onChangePassword: (passwordData: ChangePasswordPostRequest) => void;
+}> = ({ onChangePassword }) => {
+  const oldPasswordInputRef = useRef<HTMLInputElement>(null);
+  const newPasswordInputRef = useRef<HTMLInputElement>(null);
+
+  const submitHandler = (event: FormEvent) => {
+    event.preventDefault();
+
+    const oldPassword = oldPasswordInputRef.current.value;
+    const newPassword = newPasswordInputRef.current.value;
+
+    if (!oldPassword || !newPassword) return;
+
+    onChangePassword({
+      oldPassword,
+      newPassword,
+    });
+  };
+
+  return (
+    <form className={classes.form} onSubmit={submitHandler}>
+      <div className={classes.control}>
+        <label htmlFor="new-password">New Password</label>
+        <input type="password" id="new-password" ref={oldPasswordInputRef} />
+      </div>
+      <div className={classes.control}>
+        <label htmlFor="old-password">Old Password</label>
+        <input type="password" id="old-password" ref={newPasswordInputRef} />
+      </div>
+      <div className={classes.action}>
+        <button type="submit">Change Password</button>
+      </div>
+    </form>
+  );
+};
 
 export default ProfileForm;
--- components/profile/user-profile.tsx
+++ components/profile/user-profile.tsx
@@ -1,13 +1,32 @@
 import { NextPage } from 'next';
 
+import { ChangePasswordPostRequest } from '../../pages/api/user/change-password';
 import ProfileForm from './profile-form';
 import classes from './user-profile.module.css';
 
-const UserProfile: NextPage = () => (
-  <section className={classes.profile}>
-    <h1>Your User Profile</h1>
-    <ProfileForm />
-  </section>
-);
+const UserProfile: NextPage = () => {
+  const changePasswordHandler = async (
+    passwordData: ChangePasswordPostRequest
+  ) => {
+    const response = await fetch('/api/user/change-password', {
+      method: 'PATCH',
+      body: JSON.stringify(passwordData),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+
+    const data = await response.json();
+
+    console.log(data);
+  };
+
+  return (
+    <section className={classes.profile}>
+      <h1>Your User Profile</h1>
+      <ProfileForm onChangePassword={changePasswordHandler} />
+    </section>
+  );
+};
 
 export default UserProfile;
