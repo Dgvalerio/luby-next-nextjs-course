03b8cc4195bf11c4db4865a69558c048b5953b59
Davi Gonçalves Valério <al.davigvalerio@gmail.com>
2021-08-30 12:10:06 -0300

    225. Adding Server-Side Page Guards (And When To Use Which Approach)

--- components/profile/user-profile.tsx
+++ components/profile/user-profile.tsx
@@ -1,29 +1,13 @@
-import { useEffect, useState } from 'react';
-
 import { NextPage } from 'next';
-import { getSession } from 'next-auth/client';
 
 import ProfileForm from './profile-form';
 import classes from './user-profile.module.css';
 
-const UserProfile: NextPage = () => {
-  const [isLoading, setIsLoading] = useState(true);
-
-  useEffect(() => {
-    getSession().then((session) => {
-      if (!session) window.location.href = '/auth';
-      else setIsLoading(false);
-    });
-  }, []);
-
-  if (isLoading) return <p className={classes.profile}>Loading...</p>;
-
-  return (
-    <section className={classes.profile}>
-      <h1>Your User Profile</h1>
-      <ProfileForm />
-    </section>
-  );
-};
+const UserProfile: NextPage = () => (
+  <section className={classes.profile}>
+    <h1>Your User Profile</h1>
+    <ProfileForm />
+  </section>
+);
 
 export default UserProfile;
--- pages/profile.tsx
+++ pages/profile.tsx
@@ -1,7 +1,22 @@
-import { NextPage } from 'next';
+import { GetServerSideProps, NextPage } from 'next';
+import { getSession } from 'next-auth/client';
 
 import UserProfile from '../components/profile/user-profile';
 
 const ProfilePage: NextPage = () => <UserProfile />;
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const session = await getSession({ req: context.req });
+
+  if (!session)
+    return {
+      redirect: {
+        destination: '/auth',
+        permanent: false,
+      },
+    };
+
+  return { props: { session } };
+};
 
 export default ProfilePage;
