4eaaa66c008f616fe8793fbf55a7cf4bbe6e6009
Davi Gonçalves Valério <al.davigvalerio@gmail.com>
2021-08-30 12:44:06 -0300

    230. Adding the "Change Password" Logic

--- pages/api/user/change-password.ts
+++ pages/api/user/change-password.ts
@@ -1,9 +1,12 @@
 import { getSession } from 'next-auth/client';
 
+import { hashPassword, verifyPassword } from '../../../lib/auth';
+import { connectToDatabase } from '../../../lib/db';
 import { ApiHandler, ApiHandlerRequest } from '../../../types/api';
 
 export interface ChangePasswordPostRequest {
-  password: string;
+  oldPassword: string;
+  newPassword: string;
 }
 
 export interface ChangePasswordPostResponse {
@@ -27,7 +30,44 @@ const handler: ApiHandler<ChangePasswordApiRequest, ChangePasswordApiResponse> =
 
     if (!session) {
       res.status(401).json({ data: { message: 'Not authenticated!' } });
+      return;
     }
+
+    const userEmail = session.user.email;
+    const { oldPassword, newPassword } = req.body;
+
+    const client = await connectToDatabase();
+
+    const usersCollection = client.db().collection('users');
+
+    const user = await usersCollection.findOne({ email: userEmail });
+
+    if (!user) {
+      res.status(404).json({ data: { message: 'User not found!' } });
+      client.close();
+      return;
+    }
+
+    const currentPassword = user.password;
+
+    const passwordAreEqual = await verifyPassword(oldPassword, currentPassword);
+
+    if (!passwordAreEqual) {
+      res.status(403).json({ data: { message: 'Invalid password!' } });
+      client.close();
+      return;
+    }
+
+    const hashedPassword = await hashPassword(newPassword);
+
+    const result = await usersCollection.updateOne(
+      { email: userEmail },
+      { $set: { password: hashedPassword } }
+    );
+
+    client.close();
+
+    res.status(200).json({ data: { message: 'Password updated!' } });
   };
 
 export default handler;
